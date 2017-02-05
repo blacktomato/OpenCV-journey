7da2567b01b2237e40a2a53ea310604e7c75e47a
blacktomato <s90169@gmail.com>
2017-02-05 23:06:55 +0800

    adding imwrite for output the images

--- IO/IO.cpp
+++ IO/IO.cpp
@@ -2,35 +2,62 @@
  * File Name : IO.cpp
  * Purpose : IO in the OpenCV
  * Creation Date : Sun 29 Jan 2017 05:53:47 PM CST
- * Last Modified : Mon 30 Jan 2017 08:49:02 PM CST
+ * Last Modified : Sun 05 Feb 2017 07:57:27 PM CST
  * Created By : SL Chung
 **************************************************************/
 #include<iostream>
 #include<string>
-#include<opencv2/core.hpp>
 #include<opencv2/opencv.hpp>
 
 using namespace std;
 using namespace cv;
+
+bool outputJPG(string, int, Mat&);
 
 int main(int argc, char** argv)
 {
     //create a Matrix size 10x20
     //U means unsigned S means signed
     
-    Mat img1;
+    Mat img1, img2;
     img1 = imread(argv[1], IMREAD_COLOR);
+    img2 = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+    //CV_LOAD_IMAGE_GRAYSCALE == 0
+    //CV_LOAD_IMAGE_UNCHANGED == -1
+    //CV_LOAD_IMAGE_COLOR     == 1
 
     string title = "Resizable";
     
     //WINDOW_NORMAL   user can make the window manually-resizable
     //WINDOW_AUTOSIZE the size of the window will be same as the image
     
+    //imshow(const string& winname, InputArray mat)
     namedWindow(title, CV_WINDOW_NORMAL );
     imshow(title, img1);
     namedWindow("not " + title, CV_WINDOW_AUTOSIZE );
-    imshow("not " + title, img1);
-    waitKey(0); 
+    imshow("not " + title, img2);
+    
+    while(true)
+        if(waitKey(10) == 27) break; //ESC
+
+    outputJPG("95", 95, img1);
+    outputJPG("70", 70, img1);
+    outputJPG("50", 50, img1);
+    outputJPG("20", 20, img1);
+    outputJPG("10", 10, img1);
+    outputJPG( "5",  5, img1);
+    outputJPG( "4",  4, img1);
+    outputJPG( "3",  3, img1);
+    outputJPG( "2",  2, img1);
+    outputJPG( "1",  1, img1);
 
     return 0;
+}
+
+bool outputJPG(string filename, int percentage, Mat& img)
+{
+    vector<int> quality;
+    quality.push_back(CV_IMWRITE_JPEG_QUALITY);
+    quality.push_back(percentage);
+    return imwrite (filename + ".jpg", img, quality);
 }
